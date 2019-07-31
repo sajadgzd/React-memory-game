bbedc47c6402929d343747143738a7bed8fcb3c0
sajad1993gh <sajad1993gh@gmail.com>
2019-07-30 23:25:07 -0400

    pushing the app

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import "./style.css";
+import "./Card.css";
 
 const Card = props => (
   <div 
--- src/components/Wrapper/index.js
+++ src/components/Wrapper/index.js
@@ -1,8 +1,6 @@
 import React from "react";
-import "./style.css";
+import "./Wrapper.css";
 
-function Wrapper(props) {
-  return <div className="wrapper">{props.children}</div>;
-}
+const Wrapper = props => <div className="wrapper">{props.children}</div>;
 
 export default Wrapper;
